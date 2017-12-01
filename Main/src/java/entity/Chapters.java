4b95631c1e0b46ac03efba5ada4a80804d8ce956
sasha96 <sasha80630645484@ukr.net>
2017-12-01 14:01:16 +0200

    db

---
+++ Main/src/java/entity/Chapters.java
@@ -0,0 +1,27 @@
+package entity;
+
+public class Chapters {
+    private int id_chapter;
+    private String name;
+
+    public Chapters(int id_chapter, String name) {
+        this.id_chapter = id_chapter;
+        this.name = name;
+    }
+
+    public int getId_chapter() {
+        return id_chapter;
+    }
+
+    public void setId_chapter(int id_chapter) {
+        this.id_chapter = id_chapter;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
