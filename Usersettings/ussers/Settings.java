e95dc13fb864ea17e0772ce895f2a5ba58cd6f0f
ImDyA <vasurman13@gmail.com>
2017-12-06 14:57:00 +0200

    MyCommit

---
+++ Usersettings/ussers/Settings.java
@@ -0,0 +1,38 @@
+public class Settings {
+    private int ID;
+    private int ussersId;
+    private String settingsName;
+    private String value;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getUssersId() {
+        return ussersId;
+    }
+
+    public void setUssersId(int ussersId) {
+        this.ussersId = ussersId;
+    }
+
+    public String getSettingsName() {
+        return settingsName;
+    }
+
+    public void setSettingsName(String settingsName) {
+        this.settingsName = settingsName;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
---
+++ Usersettings/ussers/Usser.java
@@ -0,0 +1,38 @@
+public class Usser {
+    private int ID;
+    private int ussersId;
+    private int date;
+    private String name;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getUssersId() {
+        return ussersId;
+    }
+
+    public void setUssersId(int ussersId) {
+        this.ussersId = ussersId;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
